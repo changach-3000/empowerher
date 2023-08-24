5019bc987d02da0a7457f508e1ac136e586dee20
changach-3000 <sharon.changach@student.moringaschool.com>
2023-08-25 02:06:21 +0300

    add resources and stories page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LayoutPage from "./pages/LayoutPage";
 import LandingPage from "./pages/LandingPage";
+import Resources from "./pages/Resources";
+import NextSteps from "./pages/NextSteps";
 
 function App() {
   return (
@@ -9,7 +11,8 @@ function App() {
         <Routes>
           <Route path="/" element={<LayoutPage/>}>
           <Route index element={<LandingPage/>}/>
-          
+          <Route path="resources" element={<Resources />}/>
+          <Route path="stories" element={<NextSteps />}/>
           </Route>
         </Routes>
       </BrowserRouter>
--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -18,10 +18,10 @@ function NavBar() {
               <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-pink-500 md:p-0 dark:text-white md:dark:hover:text-pink-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent hover:underline-after text-pink-900">Resources</a>
             </li>
             <li>
-              <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-pink-500 md:p-0 dark:text-white md:dark:hover:text-pink-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent hover:underline-after text-pink-900">Next Steps</a>
+              <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-pink-500 md:p-0 dark:text-white md:dark:hover:text-pink-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent hover:underline-after text-pink-900">Stories</a>
             </li>
             <li>
-              <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-pink-500 md:p-0 dark:text-white md:dark:hover:text-pink-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent hover:underline-after text-pink-900">Stories</a>
+              <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-pink-500 md:p-0 dark:text-white md:dark:hover:text-pink-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent hover:underline-after text-pink-900">Helpline</a>
             </li>
           </ul>
         </div>
