0d42ee9f23eaddcb8b6d6fb956a291eedf83f96c
changach-3000 <sharon.changach@student.moringaschool.com>
2023-08-25 01:13:02 +0300

    finished landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,11 +2,35 @@ import React from 'react'
 
 function LandingPage() {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <section class="flex items-center justify-center bg-pink-200 h-screen dark:bg-gray-900">
+   
+    <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+    <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+        <div class="blob"></div>
+       <img src="./landing2.png" alt="pregnant girl"/>
+        </div>   
+        
+        <div class="place-self-center lg:col-span-7">
+            <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">em<span class= "text-pink-900">POWER</span><span class="font-light">her</span></h1>
+            <p class="max-w-2xl mb-6 font-light text-pink-900 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Our mission is to provide a safe and empowering space where young mothers can access vital resources, gain knowledge, and connect with a community of like-minded individuals who understand the challenges they face. We do this through curated content, expert guidance, and peer support,</p>
+            <a href="#" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+                Get started
+                <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+            </a>
+            <a href="#" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-pink-900 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                Go to site
+            </a> 
+        </div>
+
+
     </div>
+</section>
+
+
+
+
+
+
   )
 }
 
--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -2,7 +2,31 @@ import React from 'react'
 
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <nav className="border-gray-200 bg-pink-200 dark:bg-gray-800 dark:border-gray-700">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="#" className="flex items-center">
+          <img src="https://img.icons8.com/external-flat-chattapat-/64/external-baby-medical-flat-chattapat-.png" className="h-8 mr-3" alt="Flowbite Logo" />
+          <span className="self-center text-pink-900 text-2xl font-light whitespace-nowrap dark:text-white">EmpowerHer</span>
+        </a>
+        {/* ... */}
+        <div className="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
+          <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
+            <li>
+            <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-pink-500 md:p-0 dark:text-white md:dark:hover:text-pink-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent hover:underline-after text-pink-900">Home</a>
+            </li>
+            <li>
+              <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-pink-500 md:p-0 dark:text-white md:dark:hover:text-pink-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent hover:underline-after text-pink-900">Resources</a>
+            </li>
+            <li>
+              <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-pink-500 md:p-0 dark:text-white md:dark:hover:text-pink-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent hover:underline-after text-pink-900">Next Steps</a>
+            </li>
+            <li>
+              <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-pink-500 md:p-0 dark:text-white md:dark:hover:text-pink-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent hover:underline-after text-pink-900">Stories</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   )
 }
 
