cbe96ad843fad0998a9c6b83bfd7e5ba21fcafd4
changach-3000 <sharon.changach@student.moringaschool.com>
2023-08-25 15:51:47 +0300

    finished on shelters and hospitals

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import LayoutPage from "./pages/LayoutPage";
 import LandingPage from "./pages/LandingPage";
 import Resources from "./pages/Resources";
 import NextSteps from "./pages/NextSteps";
+import Hospitals from "./pages/Hospitals";
+import Shelters from "./pages/Shelters";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
           <Route index element={<LandingPage/>}/>
           <Route path="resources" element={<Resources />}/>
           <Route path="stories" element={<NextSteps />}/>
+          <Route path="hospitals" element={<Hospitals/>}/>
+          <Route path="shelters" element={<Shelters/>}/>
           </Route>
         </Routes>
       </BrowserRouter>
--- src/pages/Resources.jsx
+++ src/pages/Resources.jsx
@@ -13,7 +13,7 @@ function Resources() {
                 <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
                     <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
                 </div>
-                <h3 class="mb-2 text-xl font-bold dark:text-white">Pregnancy Journey</h3>
+               <a href='https://www.plannedparenthood.org/learn/pregnancy' target="_blank" > <h3 class="mb-2 text-xl font-bold dark:text-white">Pregnancy Journey</h3></a>
                 <p class="text-gray-500 dark:text-gray-400">Let's get to see what stage you are at, and how you can proceed learning the specific routines you need to follow with regards to diet, exercise and general body wellness for you and your baby</p>
             </div>
             <div>
@@ -41,14 +41,14 @@ function Resources() {
                 <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
                     <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z"></path></svg>
                 </div>
-                <h3 class="mb-2 text-xl font-bold dark:text-white">Family Planning</h3>
+                <h3 class="mb-2 text-xl font-bold dark:text-white">Counselling</h3>
                 <p class="text-gray-500 dark:text-gray-400">Learn how you can be better prepared for situations later on in life.</p>
             </div>
             <div>
                 <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
                     <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd"></path></svg>
                 </div>
-                <h3 class="mb-2 text-xl font-bold dark:text-white">Sexual Education</h3>
+                <h3 class="mb-2 text-xl font-bold dark:text-white">Sexual Education & family planning</h3>
                 <p class="text-gray-500 dark:text-gray-400">You can get information about your sexual health, learn more about your body, how to take care of it and learn more on sexual education.</p>
             </div>
         </div>
