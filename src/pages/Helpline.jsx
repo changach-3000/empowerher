5c313b5d4b7d4e796297d820159ab0d761f23397
changach-3000 <sharon.changach@student.moringaschool.com>
2023-08-29 16:29:52 +0300

    fix sms feature

--- src/pages/Helpline.jsx
+++ src/pages/Helpline.jsx
@@ -1,45 +1,9 @@
-import React, { useState } from "react";
-import axios from "axios"; 
-
 function Helpline() {
-  const [phoneNumber, setPhoneNumber] = useState("");
 
-  const handlePhoneNumberChange = (event) => {
-    setPhoneNumber(event.target.value);
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    // Use your actual Africa's Talking API key and other configuration
-    const apiKey = "0f2594c42b8069b1457e49b3167c196ba6869b3bced2e0b0e717c4f122bbfe92";
-    const apiUrl = "https://api.africastalking.com/version1/messaging";
-    
-    try {
-      const response = await axios.post(
-        apiUrl,
-        {
-          to: phoneNumber,
-          message: "Hello from EmpowerHer!",
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
-            apiKey,
-          },
-        }
-      );
-
-      if (response.data.SMSMessageData.Recipients.length > 0) {
-        alert("Message sent successfully!");
-      } else {
-        alert("Message sending failed.");
-      }
-    } catch (error) {
-      alert("An error occurred while sending the message.");
-    }
-  };
-
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+         
+        };
   return (
     <section className="bg-pink-200 dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -54,19 +18,18 @@ function Helpline() {
           <form className="mt-4 space-y-4 lg:mt-5" onSubmit={handleSubmit}>
             <div>
               <label
-                htmlFor="phone" // Change 'for' to 'htmlFor'
+                htmlFor="phone" 
                 className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
               >
                 Your phone number
               </label>
               <input
-                type="tel" // Change 'phone' to 'tel'
+                type="tel" 
                 name="phone"
                 id="phone"
                 className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="+254712345678"
-                required
-                onChange={handlePhoneNumberChange} // Use the handler here
+                
               />
             </div>
             <div class="flex items-start">
