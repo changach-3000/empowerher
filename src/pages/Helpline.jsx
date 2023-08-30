4dac3b16a36d7a30164b0bac7bdd7db892841621
changach-3000 <sharon.changach@student.moringaschool.com>
2023-08-30 17:01:49 +0300

    edit form for helpline

--- src/pages/Helpline.jsx
+++ src/pages/Helpline.jsx
@@ -1,12 +1,10 @@
 import React, { useState } from 'react';
 function Helpline() {
 
-  const[phoneNumber, setphoneNumber]= useState()
-
-    const handleSubmit = async (event) => {
-      event.preventDefault();
+    // const handleSubmit = async (event) => {
+    //   event.preventDefault();
       
-        };
+    //     };
   return (
     <section className="bg-pink-200 dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -18,7 +16,7 @@ function Helpline() {
             Don't fret! Just type in your phone number and we will send
             you messages with information to your phone.
           </p>
-          <form className="mt-4 space-y-4 lg:mt-5" onSubmit={handleSubmit}>
+          <form className="mt-4 space-y-4 lg:mt-5" >
             <div>
               <label
                 htmlFor="phone" 
@@ -32,7 +30,7 @@ function Helpline() {
                 id="phone"
                 className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="+254712345678"
-                onChange={(e)=> setphoneNumber(e.target.value)}
+                
                 
               />
             </div>
