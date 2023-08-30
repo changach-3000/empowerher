aa79497715ce28a35b2b7c5dc38c6dadb24a0b8a
changach-3000 <sharon.changach@student.moringaschool.com>
2023-08-30 12:16:39 +0300

    added ussd and edited chatbot

--- src/pages/Counselling.jsx
+++ src/pages/Counselling.jsx
@@ -7,6 +7,7 @@ class Review extends Component {
       super(props);
   
       this.state = {
+        question: '',
         name: '',
         gender: '',
         age: '',
@@ -15,7 +16,7 @@ class Review extends Component {
   
     componentWillMount() {
       const { steps } = this.props;
-      const { name, gender, age } = steps;
+      const { question, name, gender, age } = steps;
   
       this.setState({ name, gender, age });
     }
@@ -144,103 +145,19 @@ export const SimpleForm = () => {
              steps={[
                 {
                   id: '1',
-                  message: 'What is your name?',
-                  trigger: 'name',
+                  message: 'Hi, I am EmpowerBot! How can I help you today?',
+                  trigger: 'question',
                 },
                 {
-                  id: 'name',
+                  id: 'question',
                   user: true,
-                  trigger: '3',
+                  trigger: '2',
                 },
                 {
-                  id: '3',
-                  message: 'Hi {previousValue}! What is your gender?',
-                  trigger: 'gender',
-                },
-                {
-                  id: 'gender',
-                  options: [
-                    { value: 'male', label: 'Male', trigger: '5' },
-                    { value: 'female', label: 'Female', trigger: '5' },
-                  ],
-                },
-                {
-                  id: '5',
-                  message: 'How old are you?',
-                  trigger: 'age',
-                },
-                {
-                  id: 'age',
-                  user: true,
-                  trigger: '7',
-                  validator: (value) => {
-                    if (isNaN(value)) {
-                      return 'value must be a number';
-                    } else if (value < 0) {
-                      return 'value must be positive';
-                    } else if (value > 120) {
-                      return `${value}? Come on!`;
-                    }
-      
-                    return true;
-                  },
-                },
-                {
-                  id: '7',
-                  message: 'Great! Check out your summary',
-                  trigger: 'review',
-                },
-                {
-                  id: 'review',
-                  component: <Review />,
-                  asMessage: true,
-                  trigger: 'update',
-                },
-                {
-                  id: 'update',
-                  message: 'Would you like to update some field?',
-                  trigger: 'update-question',
-                },
-                {
-                  id: 'update-question',
-                  options: [
-                    { value: 'yes', label: 'Yes', trigger: 'update-yes' },
-                    { value: 'no', label: 'No', trigger: 'end-message' },
-                  ],
-                },
-                {
-                  id: 'update-yes',
-                  message: 'What field would you like to update?',
-                  trigger: 'update-fields',
-                },
-                {
-                  id: 'update-fields',
-                  options: [
-                    { value: 'name', label: 'Name', trigger: 'update-name' },
-                    { value: 'gender', label: 'Gender', trigger: 'update-gender' },
-                    { value: 'age', label: 'Age', trigger: 'update-age' },
-                  ],
-                },
-                {
-                  id: 'update-name',
-                  update: 'name',
-                  trigger: '7',
-                },
-                {
-                  id: 'update-gender',
-                  update: 'gender',
-                  trigger: '7',
-                },
-                {
-                  id: 'update-age',
-                  update: 'age',
-                  trigger: '7',
-                },
-                {
-                  id: 'end-message',
-                  message: 'Thanks! Your data was submitted successfully!',
-                  end: true,
-                },
+                  id: '2',
+                  message: 'For this, It is normal in pregnant women to experience this, and you should not be worried.For further assistance, look at our resources page under the pregnancy page.',
+                  // trigger: 'end-message',
+                }
               ]}
 
             opened={chatVisible}
--- src/ussd.js
+++ src/ussd.js
@@ -1,32 +1,34 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/', (req, res) => {
-    const { sessionId, serviceCode, phoneNumber, text } = req.body;
-    let response = '';
+app.post("/", (req, res) => {
+  const { sessionId, serviceCode, phoneNumber, text } = req.body;
+  let response = "";
 
-    if (text === '') {
-        response = 'CON What would you like to check\n1. My account\n2. My phone number';
-    } else if (text === '1') {
-        response = 'CON Choose account information you want to view\n1. Account number';
-    } else if (text === '2') {
-        response = `END Your phone number is ${phoneNumber}`;
-    } else if (text === '1*1') {
-        const accountNumber = 'ACC100101';
-        response = `END Your account number is ${accountNumber}`;
-    } else {
-        response = 'END Invalid input';
-    }
+  if (text === "") {
+    response =
+      "CON What would you like to check\n1. My account\n2. My phone number";
+  } else if (text === "1") {
+    response =
+      "CON Choose account information you want to view\n1. Account number";
+  } else if (text === "2") {
+    response = `END Your phone number is ${phoneNumber}`;
+  } else if (text === "1*1") {
+    const accountNumber = "ACC100101";
+    response = `END Your account number is ${accountNumber}`;
+  } else {
+    response = "END Invalid input";
+  }
 
-    res.set('Content-Type', 'text/plain');
-    res.send(response);
+  res.set("Content-Type", "text/plain");
+  res.send(response);
 });
 
 const port = 3001;
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
