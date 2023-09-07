ae37a13087bda83f698915117bc6c5ab55a8eb61
changach-3000 <sharon.changach@student.moringaschool.com>
2023-09-07 15:51:23 +0300

    add login route

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Shelters from "./pages/Shelters";
 import {SimpleForm} from "./pages/Counselling";
 import Helpline from "./pages/Helpline";
 import AboutUs from "./pages/AboutUs";
-const path = require('path-browserify');
+import Login from "./AuthPages/Login"
 
 
 
@@ -17,6 +17,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path="/login" element={<Login/>}></Route>
           <Route path="/" element={<LayoutPage/>}>
           <Route index element={<LandingPage/>}/>
           <Route path="resources" element={<Resources />}/>
@@ -26,6 +27,7 @@ function App() {
           <Route path="counselling" element={<SimpleForm/>}/>
           <Route path="helpline" element={<Helpline/>}/>
           <Route path="about" element={<AboutUs/>}/>
+          
           </Route>
         </Routes>
       </BrowserRouter>
