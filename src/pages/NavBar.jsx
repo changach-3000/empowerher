71f18774eabfe07d949efa9509e6243ef08ac8d8
changach-3000 <sharon.changach@student.moringaschool.com>
2023-09-09 10:56:24 +0300

    remove stories from navbar and add donations as part of the navbar

--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -19,13 +19,10 @@ function NavBar() {
               <Link to="resources" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-pink-500 md:p-0 dark:text-white md:dark:hover:text-pink-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent hover:underline-after text-pink-900">Resources</Link>
             </li>
             <li>
-              <Link to="stories" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-pink-500 md:p-0 dark:text-white md:dark:hover:text-pink-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent hover:underline-after text-pink-900">Stories</Link>
-            </li>
-            <li>
               <Link to="helpline" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-pink-500 md:p-0 dark:text-white md:dark:hover:text-pink-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent hover:underline-after text-pink-900">Helpline</Link>
             </li>
             <li>
-              <Link to="helpline" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-pink-500 md:p-0 dark:text-white md:dark:hover:text-pink-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent hover:underline-after text-pink-900">Donate</Link>
+              <Link to="donations" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-pink-500 md:p-0 dark:text-white md:dark:hover:text-pink-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent hover:underline-after text-pink-900">Donate</Link>
             </li>
           </ul>
         </div>
