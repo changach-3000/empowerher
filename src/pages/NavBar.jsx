0b356a9c0d5dc835d4f9cb8666976a6889801c34
changach-3000 <sharon.changach@student.moringaschool.com>
2023-09-09 09:46:18 +0300

    Add donation

--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -24,6 +24,9 @@ function NavBar() {
             <li>
               <Link to="helpline" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-pink-500 md:p-0 dark:text-white md:dark:hover:text-pink-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent hover:underline-after text-pink-900">Helpline</Link>
             </li>
+            <li>
+              <Link to="helpline" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-pink-500 md:p-0 dark:text-white md:dark:hover:text-pink-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent hover:underline-after text-pink-900">Donate</Link>
+            </li>
           </ul>
         </div>
       </div>
