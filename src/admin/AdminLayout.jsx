fffabf4d77cb7baad47523e6cd81fdd44d637531
changach-3000 <sharon.changach@student.moringaschool.com>
2023-09-10 23:07:11 +0300

    create a layout page for the admin components

---
+++ src/admin/AdminLayout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './Header'
+import SideBar from './SideBar'
+import { Outlet } from 'react-router'
+import Main from './Main'
+
+function AdminLayout() {
+  return (
+    <div>
+        {/* <Header />
+        <SideBar />
+        <Outlet /> */}
+    </div>
+  )
+}
+
+export default AdminLayout
