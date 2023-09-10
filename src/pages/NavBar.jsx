0f918aa4c6e150f4145ff5d40edb87803abbd8fb
changach-3000 <sharon.changach@student.moringaschool.com>
2023-09-11 00:22:41 +0300

    edit dropdown menu for profile

--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -1,82 +1,198 @@
-import React, { useContext, useState } from 'react'
-import { Link } from 'react-router-dom'
-import { AuthContext } from '../context/AuthContext'
+import React, { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
 
 function NavBar() {
-
   const { logout, currentuser } = useContext(AuthContext);
- 
-  return (
 
-  <nav class="bg-pink-200">
-  <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-    <div class="relative flex h-16 items-center justify-between">
-      <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-        {/* <!-- Mobile menu button--> */}
-        <button type="button" class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
-          <span class="absolute -inset-0.5"></span>
-          <span class="sr-only">Open main menu</span>
-         
-          <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-          </svg>
-       
-          <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-          </svg>
-        </button>
-      </div>
-      <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-        <div class="flex flex-shrink-0 items-center">
-          <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company"/>EmpowerHer
-        </div>
-        <div class="hidden sm:ml-6 sm:block">
-          <div class="flex space-x-4">
-            {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
-            
-            {currentuser && currentuser ? (
-                <>
-                  <Link to="/about" className="text-pink-900 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</Link>
-                  <Link to="/resources" className="text-pink-900 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Resources</Link>
-                  <Link to="/donations" className="text-pink-900 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Donate</Link>
-                  <Link to="/helpline" className="text-pink-900 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">HelpLine</Link>
-                </>
-              ) : (
-                <Link to="/about" className="text-pink-900 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</Link>
-              )}
-            {
-          currentuser && currentuser.is_admin?
-          <>
-          
-          <Link to="/dashboard" className="text-pink-900 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Dashboard</Link>
-      
-          </>:" "
-        }
-          </div>
-        </div>
-      </div>
-      <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-        {/* <!-- Profile dropdown --> */}
-        <div class="relative ml-3">
-          <div>
-            <button type="button" class="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-              <span class="absolute -inset-1.5"></span>
-              <img class="h-8 w-8 rounded-full" src="https://motherswithoutborders.org/wp-content/uploads/2022/04/woman-with-green.png" alt=""/>
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen((prevIsDropdownOpen) => !prevIsDropdownOpen);
+  };
+
+  return (
+    <nav class="bg-pink-200">
+      <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div class="relative flex h-16 items-center justify-between">
+          <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            {/* <!-- Mobile menu button--> */}
+            <button
+              type="button"
+              class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+              aria-controls="mobile-menu"
+              aria-expanded="false"
+            >
+              <span class="absolute -inset-0.5"></span>
+              <span class="sr-only">Open main menu</span>
+
+              <svg
+                class="block h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                />
+              </svg>
+
+              <svg
+                class="hidden h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
             </button>
           </div>
+          <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+            <div class="flex flex-shrink-0 items-center">
+              <img
+                class="h-8 w-auto"
+                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                alt="Your Company"
+              />
+              EmpowerHer
+            </div>
+            <div class="hidden sm:ml-6 sm:block">
+              <div class="flex space-x-4">
+                {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
 
-          <div class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
-            {/* <!-- Active: "bg-gray-100", Not Active: "" --> */}
-            <a onClick={()=>logout()} class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
-             <p class="block px-4 py-2 text-sm text-gray-700" >{ currentuser && currentuser.username}</p>
+                {currentuser && currentuser ? (
+                  <>
+                    <Link
+                      to="/about"
+                      className="text-pink-900 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                      aria-current="page"
+                    >
+                      Home
+                    </Link>
+                    <Link
+                      to="/resources"
+                      className="text-pink-900 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                    >
+                      Resources
+                    </Link>
+                    <Link
+                      to="/donations"
+                      className="text-pink-900 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                    >
+                      Donate
+                    </Link>
+                    <Link
+                      to="/helpline"
+                      className="text-pink-900 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                    >
+                      HelpLine
+                    </Link>
+                  </>
+                ) : (
+                  <Link
+                    to="/about"
+                    className="text-pink-900 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                    aria-current="page"
+                  >
+                    Home
+                  </Link>
+                )}
+                {currentuser && currentuser.is_admin ? (
+                  <>
+                    <Link
+                      to="/dashboard"
+                      className="text-pink-900 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                      aria-current="page"
+                    >
+                      Dashboard
+                    </Link>
+                  </>
+                ) : (
+                  " "
+                )}
+              </div>
+            </div>
+          </div>
+          <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            {/* <!-- Profile dropdown --> */}
+            <div class="relative ml-3">
+              <div>
+              <button
+                  type="button"
+                  onClick={toggleDropdown}
+                  className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                  id="user-menu-button"
+                  aria-expanded={isDropdownOpen}
+                  aria-haspopup="true"
+                >
+                  <span className="absolute -inset-1.5"></span>
+                  <img
+                    className="h-8 w-8 rounded-full"
+                    src="https://motherswithoutborders.org/wp-content/uploads/2022/04/woman-with-green.png"
+                    alt=""
+                  />
+                </button>
+              </div>
+              {isDropdownOpen && (
+                <div
+                  className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                  role="menu"
+                  aria-orientation="vertical"
+                  aria-labelledby="user-menu-button"
+                  tabIndex="-1"
+                >
+                  {/* Active: "bg-gray-100", Not Active: "" */}
+                  <a
+                    onClick={() => logout()}
+                    className="block px-4 py-2 text-sm text-gray-700"
+                    role="menuitem"
+                    tabIndex="-1"
+                    id="user-menu-item-2"
+                  >
+                    Sign out
+                  </a>
+                  <p className="block px-4 py-2 text-sm text-gray-700">
+                    {currentuser && currentuser.username}
+                  </p>
+                </div>
+              )}
+              {/* <div
+                class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                role="menu"
+                aria-orientation="vertical"
+                aria-labelledby="user-menu-button"
+                tabindex="-1"
+              >
+             
+                <a
+                  onClick={() => logout()}
+                  class="block px-4 py-2 text-sm text-gray-700"
+                  role="menuitem"
+                  tabindex="-1"
+                  id="user-menu-item-2"
+                >
+                  Sign out
+                </a>
+                <p class="block px-4 py-2 text-sm text-gray-700">
+                  {currentuser && currentuser.username}
+                </p>
+              </div> */}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
-</nav>
-  )
+    </nav>
+  );
 }
 
-export default NavBar
-
+export default NavBar;
