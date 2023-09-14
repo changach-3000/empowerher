2f04d0b5378d072d5a2e533336af4d659051f24d
changach-3000 <sharon.changach@student.moringaschool.com>
2023-09-14 22:00:36 +0300

    remove logo from name title

--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -59,11 +59,11 @@ function NavBar() {
           </div>
           <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div class="flex flex-shrink-0 items-center">
-              <img
+              {/* <img
                 class="h-8 w-auto"
                 src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
                 alt="Your Company"
-              />
+              /> */}
               EmpowerHer
             </div>
             <div class="hidden sm:ml-6 sm:block">
